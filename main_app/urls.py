a4b22f797d7b990ba2adfbf4ee275360c2613e8d
mikega1354 <tabrizimike@gmail.com>
2023-12-20 18:04:17 -0800

    Adds edit functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('violins/',views.violins_index, name='index'),
     path('violins/<int:violin_id>/', views.violins_detail, name='detail'),
     path('violins/create/', views.ViolinCreate.as_view(),name='violins_create'),
-
+    path('violins/<int:pk>/update/', views.ViolinUpdate.as_view(), name='violins_update'),
+    path('violins/<int:pk>/delete/', views.ViolinDelete.as_view(), name='violins_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
 from .models import Violin
-from django.views.generic.edit import CreateView
-# violins = [
-#   {'name': 'German violin', 'provenance': 'Bebenreuth', 'year': '1970', 'inventory_no': '7710','tone':'bright,clear,rich in overtones','length':'35.8 cm','price':'$3500'},
-#   {'name': 'Antique violin from Saxony', 'provenance': 'Saxony', 'year': '1900', 'inventory_no': '2342','tone':'warm','length':'35.6 cm','price':'$4700'},
-#   {'name': 'Old violin,Czech Republic', 'provenance': 'Bohemia', 'year': '1940', 'inventory_no': '1298','tone':'rich in overtones','length':'36.2 cm','price':'$2500'}
- 
-# ]
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+
 # Create your views here.
 def home(request):
     return render(request,'home.html')
@@ -32,4 +27,12 @@ class ViolinCreate(CreateView):
   fields = '__all__'
 #   success_url = '/violins' in it's here will be override the detail page
 
+class ViolinUpdate(UpdateView):
+  model = Violin
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = '__all__'
 
+
+class ViolinDelete(DeleteView):
+  model = Violin
+  success_url = '/violins'
